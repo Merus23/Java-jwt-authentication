0b98af16fe8a5a5fd082f9e3eed45e1a853a7be5
Merus23 <mateus.satira@gmail.com>
2025-01-10 17:18:58 -0300

    feat: at UserRepository add the findByEmail method

--- src/main/java/com/example/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repositories/UserRepository.java
@@ -3,4 +3,10 @@ package com.example.login_auth_api.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.login_auth_api.models.User;
 
-public interface UserRepository extends JpaRepository<User, String> { }
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByEmail(String email);
+
+}
