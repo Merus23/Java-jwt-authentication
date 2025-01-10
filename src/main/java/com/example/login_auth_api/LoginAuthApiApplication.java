5e6157e7cf5c9545f307e6302e79273d6d6b0fc0
Merus23 <mateus.satira@gmail.com>
2025-01-09 21:22:48 -0300

    chore: add gitignore

--- src/main/java/com/example/login_auth_api/LoginAuthApiApplication.java
+++ src/main/java/com/example/login_auth_api/LoginAuthApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoginAuthApiApplication {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(LoginAuthApiApplication.class, args);
 	}
 
