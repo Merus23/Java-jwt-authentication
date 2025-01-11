ad6ff20c7d3e4d53336ca37ca5ee981e72a5f18d
Merus23 <mateus.satira@gmail.com>
2025-01-11 13:07:30 -0300

    fix: add the right scanBasePackages for mapping

--- src/main/java/com/example/login_auth_api/LoginAuthApiApplication.java
+++ src/main/java/com/example/login_auth_api/LoginAuthApiApplication.java
@@ -3,7 +3,7 @@ package com.example.login_auth_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.login-auth-api")
 public class LoginAuthApiApplication {
 
 		public static void main(String[] args) {
